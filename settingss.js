a361e29b2cb5d91a4034167873f74a800cd9855a
wisedfred <wisemandevmm@gmail.com>
2025-08-15 04:28:54 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~1n8AGLLS#3sBXNUIVY-psOu1bSOekOPzgmODJpnAwrVP8PO_XAu0",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "256742015194", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
